bf684e03ebbac4097fa216a2a8750757f21908b2
Pedro <jph_vargas@hotmail.com>
2020-12-02 17:24:49 +0000

    mem

--- ft_memset.c
+++ ft_memset.c
@@ -15,13 +15,11 @@
 void	*ft_memset(void *s, int c, size_t n)
 {
     size_t i;
-    char *ptr;
 
-    ptr = (char *)s;
     i = 0;
     while (i < n)
     {
-       ptr[i] = (char)c;
+       ((char *)s)[i] = c;
        i++;
     }
     return (s);
