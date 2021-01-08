1105e1c1fd31d585b53435d6ebdcad1c12fb3d1e
Mckane666 <jph_vargas@hotmail.com>
2021-01-08 11:33:48 +0000

    alldone

---
+++ ft_lstmap.c
@@ -0,0 +1,22 @@
+
+#include "libft.h"
+t_list  *ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
+{
+    t_list  *new;
+    t_list  *temp;
+
+    new = NULL;
+    while (lst)
+    {
+        temp = ft_lstnew((*f)(lst->content));
+        if (!temp)
+        {
+            ft_lstclear(&new, del);
+            return (NULL);
+        }
+        ft_lstadd_back(&new, temp);
+        temp = temp->next;
+        lst = lst->next;
+    }
+    return (new);
+}
